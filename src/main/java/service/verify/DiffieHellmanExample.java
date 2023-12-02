3f8722abca130ac298231744d1a7ce5ab9054f3e
mekh <developermekh@gmail.com>
2023-12-02 16:48:57 +0200

    DiffieHellman key secret

---
+++ src/main/java/service/verify/DiffieHellmanExample.java
@@ -0,0 +1,55 @@
+package service.verify;
+
+import lombok.SneakyThrows;
+
+import java.security.*;
+import java.util.Arrays;
+import javax.crypto.KeyAgreement;
+
+public class DiffieHellmanExample {
+
+    @SneakyThrows
+    private static KeyPairGenerator getKeyPair() {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DH");
+        keyPairGenerator.initialize(2048);
+
+        return keyPairGenerator;
+    }
+
+    @SneakyThrows
+    private static byte[] generateSecret(PublicKey publicKey, PrivateKey privateKey) {
+        KeyAgreement keyAgreement = KeyAgreement.getInstance("DH");
+        keyAgreement.init(privateKey);
+        keyAgreement.doPhase(publicKey, true);
+
+        return keyAgreement.generateSecret();
+    }
+
+
+    public static void main(String[] args) {
+
+        KeyPairGenerator keyPairGenerator = getKeyPair();
+
+        KeyPair keyPairA = keyPairGenerator.generateKeyPair();
+        KeyPair keyPairB = keyPairGenerator.generateKeyPair();
+
+        byte[] secretKeyA = generateSecret(keyPairB.getPublic(), keyPairA.getPrivate());
+        byte[] secretKeyB = generateSecret(keyPairA.getPublic(), keyPairB.getPrivate());
+
+        if (Arrays.equals(secretKeyA, secretKeyB)) {
+            System.out.println("Secret the same!");
+            System.out.println("Secret: " + bytesToHex(secretKeyA));
+        } else {
+            System.out.println("Invalid secret!.");
+        }
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        var result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+}
+
